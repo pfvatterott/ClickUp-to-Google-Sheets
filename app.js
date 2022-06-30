6127cbed5f6e8647a0523acd85c3310de96bb938
Paul Vatterott <pfvatterott@gmail.com>
2022-06-30 10:02:02 -0600

    setting up webhook

--- app.js
+++ app.js
@@ -1,4 +1,64 @@
 const axios = require('axios');
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const PORT = 3000
+app.use(bodyParser.json())
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
+
+app.post("/hook", (req, res) => {
+    console.log(req.body) // Call your action on the request here
+    res.status(200).end() // Responding is important
+    let task_id = req.body.task_id
+    getTask(task_id).then(getTaskRes => {
+        let list_id = getTaskRes.list.list_id
+        createTask(list_id)
+    })
+})
+
+async function getTask(task_id) {
+    try {
+       let res = await axios({
+            url: `https://api.clickup.com/api/v2/task/${task_id}`,
+            method: 'get',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': 'pk_14921511_2CJK3BBNU9C5Q6PFBD6868YOJT4ROV8J'
+            }
+        })
+        if(res.status == 200){
+            console.log(res.status)
+        }     
+        return res.data
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+
+
+async function createTask(list_id) {
+    try {
+        let res = await axios({
+            url: `https://api.clickup.com/api/v2/list/${list_id}/task`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': 'pk_14921511_2CJK3BBNU9C5Q6PFBD6868YOJT4ROV8J'
+            },
+            data: {
+                'name': 'testing123'
+            }
+        })
+        if(res.status == 200){
+            console.log(res.status)
+        }     
+        return res.data
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
 
 async function getTeamData() {
     try {
@@ -61,17 +121,45 @@ async function getTeamSpaceData(team_id) {
      }
 }
 
-
-
-
-getTeamData().then(teamRes => {
-    let team_id;
-    for (let i = 0; i < teamRes.teams.length; i++) {
-        if (teamRes.teams[i].name === 'Business') {
-            team_id = teamRes.teams[i].id
-        }
+async function createFolderlessList(space_id) {
+    try {
+        let res = await axios({
+            url: `https://api.clickup.com/api/v2/space/${space_id}/list`,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': 'pk_14921511_2CJK3BBNU9C5Q6PFBD6868YOJT4ROV8J'
+            },
+            data: {
+                'name': 'testing123'
+            }
+        })
+        if(res.status == 200){
+            console.log(res.status)
+        }     
+        return res.data
     }
-    getTeamSpaceData(team_id).then(spaceData => {
-        console.log(spaceData)
-    })
-})
+    catch (err) {
+        console.error(err);
+    }
+}
+
+
+
+
+// getTeamData().then(teamRes => {
+//     let team_id;
+//     for (let i = 0; i < teamRes.teams.length; i++) {
+//         if (teamRes.teams[i].name === 'Business') {
+//             team_id = teamRes.teams[i].id
+//         }
+//     }
+//     getTeamSpaceData(team_id).then(spaceData => {
+//         let spaces = spaceData.spaces
+//         let desiredSpace = spaces.find(o => o.name === 'Space')
+//         console.log(desiredSpace.id)
+//         createFolderlessList(desiredSpace.id).then(createFolderlessListRes => {
+//             console.log(createFolderlessListRes)
+//         })
+//     })
+// })
