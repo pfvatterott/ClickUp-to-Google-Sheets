7ac550c7b8c5779e2f79af67a6760beac82ce594
Paul Vatterott <pfvatterott@gmail.com>
2022-06-30 11:05:47 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
 
 app.post("/hook", (req, res) => {
     res.status(200).end() // Responding is important 
-    console.log(req.body.history_items)
-    if (req.body.history_items.before.status != null) { //prevent from infinite loop, lol
+    console.log(req.body.history_items.before)
+    if (req.body.history_items.before.status) { //prevent from infinite loop, lol
         let task_id = req.body.task_id
         getTask(task_id).then(getTaskRes => {
             let list_id = getTaskRes.list.id
